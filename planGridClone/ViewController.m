5cf6365294978d82f99e486f343a30c204f2ce97
rnjai <rnjailamba12@gmail.com>
2016-09-08 06:59:03 +0530

    added left and right buttons to navigation item and done with action sheet

--- planGridClone/ViewController.m
+++ planGridClone/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIActionSheetDelegate>
 
 @end
 
@@ -16,7 +16,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self setUpNavBar];
+
+   
+}
+
+-(void)setUpNavBar{
+    self.navigationItem.title = @"Projects";
+    
+    UIButton *buttonView = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    [buttonView addTarget:self action:@selector(rightButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+    [buttonView setBackgroundImage:[UIImage imageNamed:@"user.png"] forState:UIControlStateNormal];
+    UIBarButtonItem *barButton = [[UIBarButtonItem alloc] initWithCustomView:buttonView];
+    [self.navigationItem setRightBarButtonItem:barButton];
+
+
+    UIButton *buttonView1 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    [buttonView1 addTarget:self action:@selector(leftButtonClicked) forControlEvents:UIControlEventTouchUpInside];
+    [buttonView1 setBackgroundImage:[UIImage imageNamed:@"news.png"] forState:UIControlStateNormal];
+    UIBarButtonItem *barButton1 = [[UIBarButtonItem alloc] initWithCustomView:buttonView1];
+    [self.navigationItem setLeftBarButtonItem:barButton1];
+}
+
+-(void)rightButtonClicked{
+    
+    UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"Action Sheet" message:@"Using the alert controller" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    [actionSheet addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        
+        // Cancel button tappped.
+        [self dismissViewControllerAnimated:YES completion:^{
+        }];
+    }]];
+    
+    [actionSheet addAction:[UIAlertAction actionWithTitle:@"Delete" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        
+        // Distructive button tapped.
+        [self dismissViewControllerAnimated:YES completion:^{
+        }];
+    }]];
+    
+    [actionSheet addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        
+        // OK button tapped.
+        
+        [self dismissViewControllerAnimated:YES completion:^{
+        }];
+    }]];
+    
+    // Present action sheet.
+    [self presentViewController:actionSheet animated:YES completion:nil];
+    
+}
+
+-(void)leftButtonClicked{
+    
 }
 
 - (void)didReceiveMemoryWarning {
