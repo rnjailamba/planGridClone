9abaa149d9b75c795f0bacf4eab2ebdd3a867120
rnjai <rnjailamba12@gmail.com>
2016-09-16 09:44:34 +0530

    new message controller added and cancel added to it

--- planGridClone/MessageViewController.m
+++ planGridClone/MessageViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MessageViewController.h"
+#import "NewMessageViewController.h"
 
 @interface MessageViewController ()
 
@@ -22,11 +23,22 @@
 
 -(void)setUpNavBar{
     self.navigationItem.title = @"Messages";
+    UIButton *buttonView1 = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 24, 24)];
+    [buttonView1 addTarget:self action:@selector(newMessageClicked) forControlEvents:UIControlEventTouchUpInside];
+    [buttonView1 setBackgroundImage:[UIImage imageNamed:@"edit.png"] forState:UIControlStateNormal];
+    UIBarButtonItem *barButton1 = [[UIBarButtonItem alloc] initWithCustomView:buttonView1];
+    
+    [self.navigationItem setRightBarButtonItem:barButton1];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)newMessageClicked{
+    UINavigationController *navController = [[UINavigationController alloc]initWithRootViewController:[[NewMessageViewController alloc]initWithNibName:@"NewMessageViewController" bundle:nil]];
+    [self.navigationController presentViewController:navController animated:YES completion:nil];
 }
 
 /*
---
+++ planGridClone/NewMessageViewController.m
@@ -0,0 +1,49 @@
+//
+//  NewMessageViewController.m
+//  planGridClone
+//
+//  Created by Mr Ruby on 16/09/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import "NewMessageViewController.h"
+
+@interface NewMessageViewController ()
+
+@end
+
+@implementation NewMessageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setUpNavBar];
+//    [self setUpTableView];
+//    [self registerNib];
+}
+
+-(void)setUpNavBar{
+    self.navigationItem.title = @"New Message";
+    UIBarButtonItem *barButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(doneClicked)];
+    [self.navigationItem setRightBarButtonItem:barButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)doneClicked{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
